39742522d2629beb1d0aeb2481a2716b52a3581f
Victor Teo <victor.teo@fillmula.com>
2023-01-15 17:10:32 +0800

    complete user defined types

--- src/main/java/io/teocloud/teointellijidea/TeoUtil.java
+++ src/main/java/io/teocloud/teointellijidea/TeoUtil.java
@@ -11,50 +11,41 @@ import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.psi.search.FileTypeIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiTreeUtil;
+import io.teocloud.teointellijidea.psi.TeoEnumDefinition;
 import io.teocloud.teointellijidea.psi.TeoFile;
+import io.teocloud.teointellijidea.psi.TeoModelDefinition;
 import io.teocloud.teointellijidea.psi.TeoProperty;
 import org.jetbrains.annotations.NotNull;
 import java.util.*;
 
 public class TeoUtil {
 
-    /**
-     * Searches the entire project for Simple language files with instances of the Simple property with the given key.
-     *
-     * @param project current project
-     * @param key     to check
-     * @return matching properties
-     */
-    public static List<TeoProperty> findProperties(Project project, String key) {
-        List<TeoProperty> result = new ArrayList<>();
+    public static List<TeoModelDefinition> findModelDefinitions(Project project) {
+        List<TeoModelDefinition> result = new ArrayList<>();
         Collection<VirtualFile> virtualFiles =
                 FileTypeIndex.getFiles(TeoFileType.INSTANCE, GlobalSearchScope.allScope(project));
         for (VirtualFile virtualFile : virtualFiles) {
             TeoFile simpleFile = (TeoFile) PsiManager.getInstance(project).findFile(virtualFile);
             if (simpleFile != null) {
-                TeoProperty[] properties = PsiTreeUtil.getChildrenOfType(simpleFile, TeoProperty.class);
-                if (properties != null) {
-                    for (TeoProperty property : properties) {
-                        if (key.equals(property.getText())) {
-                            result.add(property);
-                        }
-                    }
+                TeoModelDefinition[] models = PsiTreeUtil.getChildrenOfType(simpleFile, TeoModelDefinition.class);
+                if (models != null) {
+                    result.addAll(List.of(models));
                 }
             }
         }
         return result;
     }
 
-    public static List<TeoProperty> findProperties(Project project) {
-        List<TeoProperty> result = new ArrayList<>();
+    public static List<TeoEnumDefinition> findEnumDefinitions(Project project) {
+        List<TeoEnumDefinition> result = new ArrayList<>();
         Collection<VirtualFile> virtualFiles =
                 FileTypeIndex.getFiles(TeoFileType.INSTANCE, GlobalSearchScope.allScope(project));
         for (VirtualFile virtualFile : virtualFiles) {
             TeoFile simpleFile = (TeoFile) PsiManager.getInstance(project).findFile(virtualFile);
             if (simpleFile != null) {
-                TeoProperty[] properties = PsiTreeUtil.getChildrenOfType(simpleFile, TeoProperty.class);
-                if (properties != null) {
-                    Collections.addAll(result, properties);
+                TeoEnumDefinition[] enums = PsiTreeUtil.getChildrenOfType(simpleFile, TeoEnumDefinition.class);
+                if (enums != null) {
+                    result.addAll(List.of(enums));
                 }
             }
         }
--- src/main/java/io/teocloud/teointellijidea/completion/providers/TeoFieldTypeCompletionProvider.java
+++ src/main/java/io/teocloud/teointellijidea/completion/providers/TeoFieldTypeCompletionProvider.java
@@ -4,17 +4,25 @@ import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.openapi.project.Project;
 import com.intellij.util.ProcessingContext;
+import io.teocloud.teointellijidea.TeoUtil;
+import io.teocloud.teointellijidea.psi.TeoEnumDefinition;
+import io.teocloud.teointellijidea.psi.TeoModelDefinition;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.icons.AllIcons;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TeoFieldTypeCompletionProvider extends CompletionProvider<CompletionParameters> {
     @Override
     protected void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext context, @NotNull CompletionResultSet result) {
-        for (LookupElementBuilder element : builtinTypes) {
-            result.addElement(element);
-        }
+        result.addAllElements(List.of(builtinTypes));
+        Project project = parameters.getOriginalFile().getProject();
+        result.addAllElements(getModelTypeCandidates(project));
+        result.addAllElements(getEnumTypeCandidates(project));
     }
 
     static LookupElementBuilder[] builtinTypes = new LookupElementBuilder[]{
@@ -30,12 +38,38 @@ public class TeoFieldTypeCompletionProvider extends CompletionProvider<Completio
             createBuiltinType("DateTime"),
     };
 
-    static LookupElementBuilder createBuiltinType(String name) {
+    private static LookupElementBuilder createBuiltinType(String name) {
         return LookupElementBuilder
                 .create(name)
                 .withPresentableText(name)
                 .withTypeText("builtin", true)
                 .withIcon(AllIcons.Nodes.Type)
                 .bold();
+    }
+
+    private static ArrayList<LookupElementBuilder> getModelTypeCandidates(Project project) {
+        final List<TeoModelDefinition> modelDefinitions = TeoUtil.findModelDefinitions(project);
+        ArrayList<LookupElementBuilder> elements = new ArrayList<>();
+        for (TeoModelDefinition modelDefinition : modelDefinitions) {
+            elements.add(LookupElementBuilder
+                    .create(modelDefinition.getModelName().getText())
+                    .withTypeText(modelDefinition.getContainingFile().getName())
+                    .withIcon(AllIcons.Nodes.ModelClass)
+                    .bold());
+        }
+        return elements;
+    }
+
+    private static ArrayList<LookupElementBuilder> getEnumTypeCandidates(Project project) {
+        final List<TeoEnumDefinition> enumDefinitions = TeoUtil.findEnumDefinitions(project);
+        ArrayList<LookupElementBuilder> elements = new ArrayList<>();
+        for (TeoEnumDefinition enumDefinition : enumDefinitions) {
+            elements.add(LookupElementBuilder
+                    .create(enumDefinition.getEnumName().getText())
+                    .withTypeText(enumDefinition.getContainingFile().getName())
+                    .withIcon(AllIcons.Nodes.Enum)
+                    .bold());
+        }
+        return elements;
     }
 }
